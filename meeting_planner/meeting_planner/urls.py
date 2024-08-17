c4c91178a0bba5b9e8a515eaf0749a805b597f84
Jared Hansen <jehansen0@gmail.com>
2024-08-17 11:01:31 -0600

    Initialize website app, add urls

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from website.views import welcome, about
+
+urlpatterns = [path("admin/", admin.site.urls), path("", welcome), path("about", about)]
